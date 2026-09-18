086c2f796ff600a84e5ce367a4035361a20943f4
kanhiaya <kanhiaya@kanha-devices.lan>
2026-09-18 12:38:19 +0530

    Changes for the images

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -6,6 +6,7 @@ import SocialMedia from "../../components/socialMedia/SocialMedia";
 import Button from "../../components/button/Button";
 import {greeting} from "../../portfolio";
 import StyleContext from "../../contexts/StyleContext";
+import profileImage from "../../assets/images/profile.jpeg";
 
 export default function Greeting() {
   const {isDark} = useContext(StyleContext);
@@ -55,7 +56,7 @@ export default function Greeting() {
           <div className="greeting-image-div">
             <img
               alt="Nidhi Kumari Profile"
-              src={require("../../assets/images/profile.jpeg")}
+              src={profileImage}
               className="profile-image"
             ></img>
           </div>
